9307a6dc55c91673a0825efd1d0f4bd673260026
DonDebo37 <adebolaadesanya54@gmail.com>
2024-11-29 11:25:30 +0000

    updated lambda function

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello updated lambda function from vscode!')
+        'body': json.dumps('Hello from the lambda function in vscode!')
     }
